7738582ac41b210c5ce3f9568d9f0a3fbbd5aa5f
Esen <s7b5en@gmail.com>
2022-06-25 17:52:35 +0600

    INITIAL COMMIT

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+
+    includeAssets: [
+      "favicon.svg",
+      "favicon.ico",
+      "robots.txt",
+      "apple-touch-icon.png",
+    ],
+    manifest: {
+      name: "slowlife",
+      short_name: "slowlife",
+      description: "slow life",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+  })],
+});
