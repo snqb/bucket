3ce2b25c767f2f6769b5c1f70c2dff33d82d8772
Esen <s7b5en@gmail.com>
2022-12-04 06:03:30 +0600

    Revert "add ssr"
    This reverts commit a758c78ed03c16b55c82fb5eaeaeb826aee4b175.

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ssr(),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
 
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-        "wave3.png",
+    includeAssets: [
+      "favicon.svg",
+      "favicon.ico",
+      "robots.txt",
+      "apple-touch-icon.png",
+      "wave3.png"
+    ],
+    manifest: {
+      name: "Bucket",
+      short_name: "Bucket",
+      description: "Bucket it!",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
       ],
-      manifest: {
-        name: "Bucket",
-        short_name: "Bucket",
-        description: "Bucket it!",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
+    },
+  })],
 });
