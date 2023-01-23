9aa86b5e7aadcc5e5d3e45f1f019566d98baf07f
Esen <s7b5en@gmail.com>
2023-01-24 03:55:04 +0600

    add some touch of beauty to sync

--- src/Bucket/Bucket.tsx
+++ src/Bucket/Bucket.tsx
@@ -1,4 +1,16 @@
-import { Accordion, Box, Button, Flex, Input, VStack } from "@chakra-ui/react";
+import {
+  Accordion,
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Adder from "../Adder";
 import Graveyard from "../Graveyard";
@@ -6,13 +18,11 @@ import Task from "../Task";
 
 import { useSyncedStore } from "@syncedstore/react";
 
-import { store, Thingy } from "../store";
+import { IconButton } from "@chakra-ui/react";
 import { useState } from "react";
+import { store, Thingy } from "../store";
 
 const Bucket = () => {
-  const [password, setPassword] = useState(
-    localStorage.getItem("password") ?? ""
-  );
   return (
     <Box position="relative" pt="3vh">
       <VStack align="stretch" gap={4}>
@@ -25,22 +35,7 @@ const Bucket = () => {
           <Graveyard />
         </Flex>
 
-        <Flex>
-          <Input
-            value={password}
-            onChange={(e) => {
-              const value = e.target.value;
-
-              if (value) {
-                localStorage.setItem("password", e.target.value);
-                setPassword(e.target.value);
-              }
-            }}
-            placeholder="sync phrase"
-          />
-          {/* @ts-ignore */}
-          <Button onClick={() => window.location.reload(true)}>Save</Button>
-        </Flex>
+        <SyncInput />
       </VStack>
     </Box>
   );
@@ -61,6 +56,52 @@ const BucketView = () => {
           })}
       </Accordion>
     </div>
+  );
+};
+
+const SyncInput = () => {
+  const [password, setPassword] = useState(
+    localStorage.getItem("password") ?? ""
+  );
+
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <Flex direction="column" gap={2} pb={5}>
+      <FormControl>
+        <FormLabel>Sync phrase</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="sync phrase"
+            onChange={(e) => {
+              const value = e.target.value;
+
+              if (value) {
+                localStorage.setItem("password", e.target.value);
+                setPassword(e.target.value);
+              }
+            }}
+            value={password}
+          />
+          <InputRightElement w="3rem">
+            <IconButton
+              variant="solid"
+              h="1.75rem"
+              size="sm"
+              icon={<>{show ? "🙉" : "🙈"}</>}
+              onClick={handleClick}
+              aria-label={"show or hide sync phrase"}
+            />
+          </InputRightElement>
+        </InputGroup>
+        <FormHelperText>
+          use this phrase on your other device to sync bucket data via web-rtc
+        </FormHelperText>
+      </FormControl>
+      <Button onClick={() => window.location.reload()}>start syncing</Button>
+    </Flex>
   );
 };
 
