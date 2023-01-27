c8d0d3cc17105724f70162d510f26e7ab482127f
Esen <s7b5en@gmail.com>
2023-01-27 17:41:19 +0600

    add cleaner

--- src/Today/Today.tsx
+++ src/Today/Today.tsx
@@ -1,7 +1,4 @@
-import {
-  Accordion,
-  Box, VStack
-} from "@chakra-ui/react";
+import { Accordion, Box, Button, VStack } from "@chakra-ui/react";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Adder from "../Adder";
 import Task from "../Task";
@@ -27,6 +24,7 @@ const TodayView = () => {
   const today = useSyncedStore(store.today);
 
   const parent = useAutoAnimate({ duration: 250, easing: "linear" });
+  const hasDone = today.some((task) => task.progress === 100);
 
   return (
     <div id="Today">
@@ -43,6 +41,28 @@ const TodayView = () => {
           );
         })}
       </Accordion>
+      {hasDone && (
+        <Button
+          w="full"
+          colorScheme="red"
+          onClick={() => {
+            const cleanup = () => {
+              const doneIndex = today.findIndex((it) => it.progress === 100);
+              if (doneIndex < 0) {
+                return;
+              }
+
+              // we recursively delete them because syncedstore doesn't support `filter`, as we have to mutate
+              today.splice(doneIndex, 1);
+              cleanup();
+            };
+
+            cleanup();
+          }}
+        >
+          🔪 Clear
+        </Button>
+      )}
     </div>
   );
 };
