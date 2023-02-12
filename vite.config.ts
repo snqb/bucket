a9efa19696cf399688993d5efd633d24a48de192
Esen <s7b5en@gmail.com>
2023-02-13 05:22:34 +0600

    include line into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,69 @@ import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const PWA = VitePWA({
+  strategies: "injectManifest",
+  srcDir: "src",
+  filename: "sw.ts",
+
+  includeAssets: [
+    "favicon-196.png",
+    "robots.txt",
+
+    "apple-splash-dark-2048-2732.jpg",
+    "apple-splash-dark-1668-2388.jpg",
+    "apple-splash-dark-1536-2048.jpg",
+    "apple-splash-dark-1668-2224.jpg",
+    "apple-splash-dark-1620-2160.jpg",
+    "apple-splash-dark-1290-2796.jpg",
+    "apple-splash-dark-1179-2556.jpg",
+    "apple-splash-dark-1284-2778.jpg",
+    "apple-splash-dark-1170-2532.jpg",
+    "apple-splash-dark-1125-2436.jpg",
+    "apple-splash-dark-1242-2688.jpg",
+    "apple-splash-dark-828-1792.jpg",
+    "apple-splash-dark-1242-2208.jpg",
+    "apple-splash-dark-750-1334.jpg",
+    "apple-splash-dark-640-1136.jpg",
+    "apple-icon-180.png",
+
+    "wave3.png",
+    "line2.svg",
+  ],
+  manifest: {
+    name: "Bucket",
+    short_name: "Bucket",
+    description: "words words",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+});
+
 export default defineConfig({
   css: {
     postcss: {
@@ -11,69 +73,10 @@ export default defineConfig({
     },
   },
   plugins: [
+    PWA,
     preact({
       include: "**/*.tsx",
     }),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
-
-      includeAssets: [
-        "favicon-196.png",
-        "robots.txt",
-
-        "apple-splash-dark-2048-2732.jpg",
-        "apple-splash-dark-1668-2388.jpg",
-        "apple-splash-dark-1536-2048.jpg",
-        "apple-splash-dark-1668-2224.jpg",
-        "apple-splash-dark-1620-2160.jpg",
-        "apple-splash-dark-1290-2796.jpg",
-        "apple-splash-dark-1179-2556.jpg",
-        "apple-splash-dark-1284-2778.jpg",
-        "apple-splash-dark-1170-2532.jpg",
-        "apple-splash-dark-1125-2436.jpg",
-        "apple-splash-dark-1242-2688.jpg",
-        "apple-splash-dark-828-1792.jpg",
-        "apple-splash-dark-1242-2208.jpg",
-        "apple-splash-dark-750-1334.jpg",
-        "apple-splash-dark-640-1136.jpg",
-        "apple-icon-180.png",
-
-        "wave3.png",
-      ],
-      manifest: {
-        name: "Bucket",
-        short_name: "Bucket",
-        description: "words words",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
+    ,
   ],
 });
