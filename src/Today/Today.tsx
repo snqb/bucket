b5f309d1ac4e603bdce7ec80b5a655182e924cc4
Esen <s7b5en@gmail.com>
2023-02-13 04:47:03 +0600

    pizdato

--- src/Today/Today.tsx
+++ src/Today/Today.tsx
@@ -20,11 +20,11 @@ const TodayView = () => {
     <>
       <Flex
         h="100%"
-        mt="20vh"
         ref={parent as any}
         gap={5}
         direction="column"
         pb="20vmin"
+        justify="end"
       >
         {hasDone && (
           <Button
