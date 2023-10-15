b1cf2160ed81a52d2d9b4aefa6afb67273c76c08
Esen <s7b5en@gmail.com>
2023-10-15 07:31:03 +0600

    test with dummy

--- src/Period.tsx
+++ src/Period.tsx
@@ -1,90 +1,163 @@
-import { ShortTask } from "./Task";
 import {
-	Box,
-	Grid,
-	HStack,
-	Heading,
-	StackDivider,
-	VStack,
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  StackDivider,
+  VStack,
+  Text,
+  Center,
 } from "@chakra-ui/react";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { ShortTask } from "./Task";
 
-import Adder from "./Adder";
+import { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import Adder, { getRandomEmoji } from "./Adder";
 import { PERIODS, PERIOD_TEXTS } from "./constants";
 import { TodoState, horizontalIndex, useAppSelector } from "./store";
-import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
-import { useEffect, useState } from "react";
-
+import { AutoTextSize } from "auto-text-size";
+import { motion } from "framer-motion";
 interface Props {
-	periods: readonly (keyof TodoState)[];
-	row: number;
+  periods: readonly (keyof TodoState)[];
+  row: number;
 }
 
 const Period = ({ periods, row }: Props) => {
-	const tasks = useAppSelector((state) => state.todo);
-	const [controller, setController] = useState<SwiperClass>();
+  const tasks = useAppSelector((state) => state.todo);
+  const [controller, setController] = useState<SwiperClass>();
 
-	const [autoAnimate] = useAutoAnimate({ duration: 250, easing: "linear" });
+  const [autoAnimate] = useAutoAnimate({ duration: 250, easing: "linear" });
 
-	useEffect(() => {
-		const timerId = requestAnimationFrame(() =>
-			controller?.slideToLoop(horizontalIndex.value, 0, false),
-		);
+  useEffect(() => {
+    const timerId = requestAnimationFrame(() =>
+      controller?.slideToLoop(horizontalIndex.value, 0, false),
+    );
 
-		return () => cancelAnimationFrame(timerId);
-	}, [horizontalIndex.value]);
+    return () => cancelAnimationFrame(timerId);
+  }, [horizontalIndex.value]);
 
-	return (
-		<Swiper
-			slidesPerView={1}
-			loop
-			onSlideChange={(it) => {
-				horizontalIndex.value = it.realIndex;
-			}}
-			onSwiper={setController}
-			direction="horizontal"
-			initialSlide={horizontalIndex.value}
-		>
-			{periods.map((period, index) => (
-				<SwiperSlide key={period} style={{ width: "100vw", height: "100vh" }}>
-					<VStack
-						spacing={2}
-						id="later"
-						align="stretch"
-						divider={
-							<StackDivider borderStyle="dotted" borderColor="gray.800" />
-						}
-						ref={autoAnimate as any}
-						maxHeight="77vh"
-					>
-						<HStack>
-							<Grid
-								w="22px"
-								h="22px"
-								templateColumns="repeat(3, 1fr)"
-								gap={0.5}
-							>
-								{PERIODS.map((it, current) => (
-									<Box
-										key={it}
-										borderRadius="50%"
-										bg={current === row * 3 + index ? "white" : "gray.500"}
-									/>
-								))}
-							</Grid>
-							<Heading>{PERIOD_TEXTS[period]}</Heading>
-						</HStack>
+  return (
+    <Swiper
+      slidesPerView={1}
+      loop
+      onSlideChange={(it) => {
+        horizontalIndex.value = it.realIndex;
+      }}
+      onSwiper={setController}
+      direction="horizontal"
+      initialSlide={horizontalIndex.value}
+    >
+      {periods.map((period, index) => (
+        <SwiperSlide key={period} style={{ width: "100vw", height: "100vh" }}>
+          <VStack
+            spacing={2}
+            id="later"
+            align="stretch"
+            divider={
+              <StackDivider borderStyle="dotted" borderColor="gray.800" />
+            }
+            ref={autoAnimate as any}
+            maxHeight="77vh"
+          >
+            <HStack>
+              <Grid
+                w="22px"
+                h="22px"
+                templateColumns="repeat(3, 1fr)"
+                gap={0.5}
+              >
+                {PERIODS.map((it, current) => (
+                  <Box
+                    key={it}
+                    borderRadius="50%"
+                    bg={current === row * 3 + index ? "white" : "gray.500"}
+                  />
+                ))}
+              </Grid>
+              <Heading>{PERIOD_TEXTS[period]}</Heading>
+            </HStack>
+            <Adder placeholder="faster things..." where={period} />
 
-						<Adder placeholder="faster things..." where={period} />
-
-						{tasks[period].map((task, index) => (
-							<ShortTask key={task.id} task={task} where={period} />
-						))}
-					</VStack>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
+            {tasks[period].map((task, index) => (
+              <ShortTask key={task.id} task={task} where={period} />
+            ))}
+          </VStack>
+          <HStack
+            position="fixed"
+            bottom="15%"
+            wrap="wrap"
+            justify="space-evenly"
+            align="space-evenly"
+            gap={2}
+          >
+            <CircleText text="milk a cow" />
+            <CircleText text="work out" />
+            <CircleText text="work an hour with" />
+            <CircleText text="pages" />
+          </HStack>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default Period;
+
+interface CircleTextProps {
+  text: string;
+}
+
+const CircleText: React.FC<CircleTextProps> = ({ text }) => {
+  const [emoji] = useState(getRandomEmoji());
+  const [number, setNumber] = useState(0);
+
+  return (
+    <Button
+      variant="ghost"
+      borderRadius="50%"
+      width="84px"
+      height="84px"
+      position="relative"
+      bg="black"
+      // outlineColor="whiteAlpha.50"
+      onClick={() => setNumber((it) => it + 1)}
+    >
+      <Box
+        sx={{
+          fontSize: "2.5rem",
+          position: "absolute",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          opacity: "0.5",
+        }}
+      >
+        {emoji}
+      </Box>
+      <MotionBox
+        background="gray.800"
+        borderRadius="50%"
+        p={1}
+        position="absolute"
+        left="40%"
+        fontSize="12px"
+        minW="24px"
+        key={number}
+        initial={{ y: -20 }}
+        animate={{ y: 0 }}
+        exit={{ y: 20 }}
+      >
+        {number}
+      </MotionBox>
+      <VStack width="64px" height="64px" align="center" justify="end">
+        {/* <Text fontSize="32px">{emoji}</Text> */}
+        <Box width="90%">
+          <AutoTextSize>{text}</AutoTextSize>
+        </Box>
+      </VStack>
+    </Button>
+  );
+};
+
+const MotionBox = motion(Box);
